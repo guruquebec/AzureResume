4b3afcaa7df9d3032ef6e4e29fbd928b664e3928
guruquebec <guruquebec@yahoo.ca>
2024-09-15 16:46:51 -0400

    created storage account and deployed the front end website html, javascript and css files to Azure

--- frontend/main.js
+++ frontend/main.js
@@ -2,10 +2,10 @@ window.addEventListener('DOMContentLoaded',(event)=> {
     getHitCount();
 })
 const funcApi = 'http://localhost:7071/api/resumecounter';
-
+const funcURL = 'https://func-resumecount.azurewebsites.net/api/resumecounter?code=0oTCFKNomF_Bd8awb8zALOwqxtGfLyctHeDfXy6sOij5AzFuOfM-zg%3D%3D'
 const getHitCount = () =>{
 let count = 40;
-fetch (funcApi).then (response => {
+fetch (funcURL).then (response => {
     return response.json()
 }).then (response => {
     console.log("Website called for count");
